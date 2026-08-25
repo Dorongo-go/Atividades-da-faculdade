af19fe4bca8f910626347b7bda7cd5daa85384fa
Otavio Morais de Freitas <omorais245@gmail.com>
2026-08-25 09:48:26 -0300

    Add files via upload

---
+++ Excício_1.py
@@ -0,0 +1,27 @@
+#Teste 1
+
+#V_1 = 4
+#V_2 = 9
+#Resultado = 9 + 4
+#print(f"A soma de {V_1} com o {V_2} é {Resultado}")
+
+#Teste 2
+#V_1 = -3
+#V_2 = 8
+#Resultado = -3 + 8
+#print(f"A soma de {V_1} com o {V_2} é {Resultado}")
+
+#Teste 3
+#V_1 = 0
+#V_2 = 12
+#Resultado = 0 + 12
+#print(f"A soma de {V_1} com o {V_2} é {Resultado}")
+
+#Final
+
+Pui = input("Olá qual é seu nome? ")
+Doria = input(f"Seu nome é {Pui}, bem me fale um número: ")
+Zarbo = input("Outro: ")
+
+soma = int(Doria) + int(Zarbo)
+Resultado =print(f"A soma entre {Doria} e {Zarbo} deu {soma}")
